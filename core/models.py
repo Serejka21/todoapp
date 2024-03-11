ffa4712905d5e6d75ab4a91acaa79e1ccb34886d
Serhii Buryk <sergmarten21@gmail.com>
2024-03-11 11:54:13 +0200

    add unique constaint to tag model on field name

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=63,)
+    name = models.CharField(max_length=63, unique=True)
 
     class Meta:
         verbose_name_plural = "Tags"
