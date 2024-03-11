c4953c914b0b7461d665a6f7d68eb3bf13bfe18a
Serhii Buryk <sergmarten21@gmail.com>
2024-03-11 11:47:59 +0200

    implement all logic of the project, add forms, templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+import datetime
+from datetime import timezone
 
-# Create your views here.
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic
+
+from core.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "core/index.html"
+    ordering = ["-done"]
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("core:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("core:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("core:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    ordering = ["name"]
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("core:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("core:tag-list")
+
+
+def mark_as_done(request: HttpRequest, pk: int) -> HttpResponse:
+    task = get_object_or_404(Task, id=pk)
+    task.done = True
+    task.save()
+    return HttpResponseRedirect(
+        request.META.get(
+            "HTTP_REFERER",
+            reverse_lazy(viewname="core:task-list"))
+    )
+
+
+def mark_as_undo(request: HttpRequest, pk: int) -> HttpResponse:
+    task = get_object_or_404(Task, id=pk)
+    task.done = False
+    task.save()
+    return HttpResponseRedirect(
+        request.META.get(
+            "HTTP_REFERER",
+            reverse_lazy(viewname="core:task-list"))
+    )
